01dc696a9cc71fea173bb862df2a36966109e849
elvismariel <elvismariel@gmail.com>
2020-01-17 16:55:17 -0300

    Frontend React JS - desenvolvimento do template em html main

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './global.css'
 import './App.css'
 import './Sidebar.css'
+import './Main.css'
 
 function App() {
   return (
@@ -35,7 +36,52 @@ function App() {
         </form>
       </aside>
       <main>
-
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/290037?s=460&v=4" alt="Elvis Carvalho Souza" />
+              <div className="user-info">
+                <strong>Elvis Carvalho Souza</strong>
+                <span>ReactJs, React Native, Node.js, Java, Php, C#</span>
+              </div>
+            </header>
+            <p>Analista de Inovação Digital. Desenvolvedor a mais de dez anos, apaixonado por inovação, construindo sistemas nas mais diversas tecnologias do mercado.</p>
+            <a href="https://github.com/elvismariel">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/290037?s=460&v=4" alt="Elvis Carvalho Souza" />
+              <div className="user-info">
+                <strong>Elvis Carvalho Souza</strong>
+                <span>ReactJs, React Native, Node.js, Java, Php, C#</span>
+              </div>
+            </header>
+            <p>Analista de Inovação Digital. Desenvolvedor a mais de dez anos, apaixonado por inovação, construindo sistemas nas mais diversas tecnologias do mercado.</p>
+            <a href="https://github.com/elvismariel">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/290037?s=460&v=4" alt="Elvis Carvalho Souza" />
+              <div className="user-info">
+                <strong>Elvis Carvalho Souza</strong>
+                <span>ReactJs, React Native, Node.js, Java, Php, C#</span>
+              </div>
+            </header>
+            <p>Analista de Inovação Digital. Desenvolvedor a mais de dez anos, apaixonado por inovação, construindo sistemas nas mais diversas tecnologias do mercado.</p>
+            <a href="https://github.com/elvismariel">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/290037?s=460&v=4" alt="Elvis Carvalho Souza" />
+              <div className="user-info">
+                <strong>Elvis Carvalho Souza</strong>
+                <span>ReactJs, React Native, Node.js, Java, Php, C#</span>
+              </div>
+            </header>
+            <p>Analista de Inovação Digital. Desenvolvedor a mais de dez anos, apaixonado por inovação, construindo sistemas nas mais diversas tecnologias do mercado.</p>
+            <a href="https://github.com/elvismariel">Acessar perfil no Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
