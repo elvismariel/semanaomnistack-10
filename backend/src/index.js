d5f1f030bda18f155318a9ce6621304e70de8f62
elvismariel <elvismariel@gmail.com>
2020-01-17 17:40:00 -0300

    Backend Node JS - habilitando acesso externo na api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const routes = require("./routers");
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
 mongoose.connect('mongodb://localhost/omnistack', {
-    useNewUrlParser: true, useUnifiedTopology: true
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 }).then(() => {
     console.log('Conectado ao mongo com sucesso!');
 }).catch((erro) => {
